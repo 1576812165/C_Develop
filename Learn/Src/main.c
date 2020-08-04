80b23622d987921d9d1a08e2703c4a9124fa776c
EmbeddedNovice <1576812165@qq.com>
2020-08-04 15:22:26 +0800

    提交基础工程

---
+++ Learn/Src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(void)
+{
+//   uint8_t *pNumber = NULL;
+//   uint16_t size = 0;
+//
+//   scanf("%d",&size);
+//
+//   pNumber = (uint8_t *)malloc(size * sizeof(uint8_t));
+//
+//   if (pNumber != NULL)
+//   {
+//       for (uint16_t i = 0;i < size;i++)
+//       {
+//           *(pNumber + i) = i;
+//       }
+//
+//       for (uint16_t i = 0;i < size;i++)
+//       {
+//           printf("%d\r\n",*pNumber++);
+//       }
+//
+//       free(pNumber);
+//       pNumber = NULL;
+//   }
+
+
+
+   return 0;
+}
+
